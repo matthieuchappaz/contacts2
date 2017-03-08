68a44381fd12ecb43f563ab5b9fd8d89d7ad191b
Matthieu Chappaz <matthieu.chappaz@gmail.com>
2017-03-08 12:19:50 +0100

    Initial Commit

---
+++ contacts2Tests/contacts2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  contacts2Tests.swift
+//  contacts2Tests
+//
+//  Created by Matthieu Chappaz on 08/03/2017.
+//  Copyright © 2017 Matthieu Chappaz. All rights reserved.
+//
+
+import XCTest
+@testable import contacts2
+
+class contacts2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
